d0c69d195a32ede66392ee57b0bc533eb2bf12d4
mo7ub <mohamedgamal657@gmail.com>
2024-12-17 22:50:04 +0200

    adding new dev branch
    and some QOL fixes

--- Fractals/KochSnowflake.java
+++ Fractals/KochSnowflake.java
@@ -2,19 +2,29 @@ package Fractals;
 
 import Explorer.FractalUpdater;
 import Panels.SnowflakePanel;
-
-import javax.swing.*;
 import java.awt.*;
+import javax.swing.*;
 
 public class KochSnowflake extends JPanel {
     private SnowflakePanel snowflakePanel;
     private JSpinner depthSpinner;
 
+    //added constatnts to make it easier to edit later on
+    private static final int INITIAL_DEPTH = 5;
+    private static final int MIN_DEPTH = 1;
+    private static final int MAX_DEPTH = 10;
+    private static final int STEP = 1;
+
     public KochSnowflake(FractalUpdater fractalUpdater) {
         setLayout(new BorderLayout());  // Use BorderLayout for consistent layout
 
+        
+
         snowflakePanel = new SnowflakePanel();
-        depthSpinner = new JSpinner(new SpinnerNumberModel(5, 1, 10, 1));
+        depthSpinner = new JSpinner(new SpinnerNumberModel(INITIAL_DEPTH, MIN_DEPTH, MAX_DEPTH, STEP));
+
+        //Added to update the panel to the desired depth as soon as it launches
+        snowflakePanel.setDepth(INITIAL_DEPTH);
 
         depthSpinner.addChangeListener(e -> {
             // Pass the selected depth to the fractal panel
